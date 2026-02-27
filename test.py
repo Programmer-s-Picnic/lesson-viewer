01e5c13fe363bc2734fd9cd528984d0b30865587
Champak's World <champaksworld@gmail.com>
2026-02-27 15:06:51 +0530

    Added voice search

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
-a = [
-    [1, 2, 23],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-leftdiagonalsum = 0
-rightdiagonalsum = 0
-n = len(a)
-for i in range(n):
-    leftdiagonalsum += a[i][i]
-    rightdiagonalsum += a[n-1-i][i]
+import matplotlib.pyplot as plt
+import pandas as pd
+data = {
+    'id': [1, 2, 3, 4, 5, 6],
+    'name': ['Dhoni', 'Piyush', 'Gambhir', 'Piyush', 'Rahul', 'Piyush'],
+    'age': [24, 27, 22, 32, 29, 35],
+    'city': ['Mumbai', 'Delhi', 'Bangalore', 'Chennai', 'Hyderabad', 'Kolkata'],
+    'score': [88, 92, 85, 70, 90, 95]
+}
 
-print(leftdiagonalsum, rightdiagonalsum)
+df = pd.DataFrame(data)
+# print(df)
+# result = df[df['age'] > 25]
+# print("Age>25\n", result)
+
+# result = df.query('score > 85')[['name', 'score']]
+# print('score > 85\n', result)
+
+# query = input()
+# result = df.query(query)[['name', 'score']]
+# print('score > 85\n', result)
+
+ages = df[["age"]]
+# print(ages)
+ids = df[["id"]]
+# print(ids)
+plt.plot(ids, ages)
+plt.show()
